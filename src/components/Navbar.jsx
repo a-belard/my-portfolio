fbbecf2773e1cf7e8ed6fc80733ccda945c05aab
a-belard <angebelard@gmail.com>
2022-05-03 21:44:25 +0200

    styles: stying navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { my_data } from "../data";
 import { navlinks } from "../data/navlinks";
 import "../styles/navbar.scss";
+import navActions from "../reducers/navigation";
 
 export default function () {
-  let active = 0;
+  const dispatch = useDispatch();
+  const active = useSelector((store) => store.navigation);
+  const navigateHandler = (navId) => dispatch(navActions.navigate(navId));
   return (
-    <nav className=" flex justify-between px-10 h-12 items-center text-xs secondary-1 primary-bg-2 font-bold">
+    <nav className=" flex justify-between px-8 h-12 items-center text-xs secondary-1 primary-bg-2 font-bold">
       <div className="flex gap-10 h-full">
         <button>_{my_data.firstName}</button>
         <div className=" flex ml-20 h-full">
@@ -14,6 +18,7 @@ export default function () {
             navlinks.map((navlink, i) => (
               <button
                 key={i}
+                onClick={(e) => navigateHandler(i)}
                 className={
                   (i === active ? "navlink-active" : "") +
                   " h-full px-8 navlink"
@@ -24,8 +29,8 @@ export default function () {
             ))}
         </div>
       </div>
-      <div>
-        <button>_contact-me</button>
+      <div className="h-full">
+        <button className="h-full contact-me-btn pl-8">_contact-me</button>
       </div>
     </nav>
   );
