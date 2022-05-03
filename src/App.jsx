8414001d61c0c8aca506bc1210855ca9d0dd6137
a-belard <angebelard@gmail.com>
2022-05-03 11:08:15 +0200

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as actions from "./actions";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ src/actions.js
@@ -0,0 +1,2 @@
+export const INCREMENT = "incremement";
+export const DECREMENT = "decrement";
