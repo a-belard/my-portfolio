14417110af576bd4a14d3910c6d963b52817fae8
a-belard <angebelard@gmail.com>
2022-05-04 21:43:56 +0200

    feat: adding new reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,15 @@
+import { active_personalInfo } from "./active_personalInfo";
+import { contacts_panel_reducer } from "./contacts_panel";
+import { education_level } from "./education_level";
+import { education_panel_reducer } from "./education_panel";
 import { navReducer } from "./navigation";
+import { personalinfo_panel_reducer } from "./personalinfo_panel";
 
 export default {
   navigation: navReducer,
+  contacts_panel: contacts_panel_reducer,
+  personalinfo_panel: personalinfo_panel_reducer,
+  education_panel: education_panel_reducer,
+  active_persInfo: active_personalInfo,
+  education_level: education_level,
 };
