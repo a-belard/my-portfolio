e61838b2c4999640c1627eb12462e05f5907ce91
a-belard <angebelard@gmail.com>
2022-05-04 21:42:31 +0200

    Update Home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Welcome from "./Welcome";
+import "remixicon/fonts/remixicon.css";
+import AboutMe from "./AboutMe.jsx/AboutMe";
+
+const subViews = [<Welcome />, <AboutMe />];
 
 export default function Home() {
+  const active = useSelector((store) => store.navigation);
   return (
-    <div>
+    <div className="md:h-screen flex flex-col gap-0">
       <Navbar />
+      {subViews[active]}
+      <Footer />
     </div>
   );
 }
