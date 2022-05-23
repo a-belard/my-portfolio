18013b3a547f23f6dab3f6b7b0620d4580b73252
a-belard <angebelard@gmail.com>
2022-05-23 09:29:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 
 function App() {
+  window.oncontextmenu = function () {
+    return false;
+  };
+
+  document.addEventListener(
+    "keydown",
+    function (event) {
+      var key = event.key || event.keyCode;
+
+      if (key == 123) {
+        return false;
+      } else if (
+        (event.ctrlKey && event.shiftKey && key == 73) ||
+        (event.ctrlKey && event.shiftKey && key == 74)
+      ) {
+        return false;
+      }
+    },
+    false
+  );
   return (
     <BrowserRouter>
       <Routes>
