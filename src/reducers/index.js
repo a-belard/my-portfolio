fe729ecae4bd9847a4cb18d8b07d117dc57a9782
a-belard <angebelard@gmail.com>
2022-05-23 09:29:59 +0200

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { active_personalInfo } from "./active_personalInfo";
+import { contactFormReducer } from "./contactForm";
 import { contacts_panel_reducer } from "./contacts_panel";
 import { education_level } from "./education_level";
 import { education_panel_reducer } from "./education_panel";
+import { languages_panel_reducer } from "./languages_panel";
 import { navReducer } from "./navigation";
 import { personalinfo_panel_reducer } from "./personalinfo_panel";
+import { projectsTechReducer } from "./projectsTech";
 
 export default {
   navigation: navReducer,
@@ -12,4 +15,7 @@ export default {
   education_panel: education_panel_reducer,
   active_persInfo: active_personalInfo,
   education_level: education_level,
+  languages_panel: languages_panel_reducer,
+  contactForm: contactFormReducer,
+  projectsTech: projectsTechReducer,
 };
